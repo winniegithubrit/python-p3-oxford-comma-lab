a2c3529fe68bb11b2e14b70403e3eda955fb4742
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-05-19 16:50:49 +0300

    Completed assignment

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
+
 def oxford_comma(items):
-    return None
+    if len(items) == 0:
+        return ""
+    elif len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        oxford_str = ", ".join(items[:-1])
+        return f"{oxford_str}, and {items[-1]}"
